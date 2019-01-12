ea7e1e0932baf920d0e6322e04e1120ac836c848
cs-eliseev <a.k.eliseev@gmail.com>
2019-01-12 09:47:34 +0300

    add examples convert month to word

--- examples/examples-word.php
+++ examples/examples-word.php
@@ -10,3 +10,10 @@ $text = mb_convert_encoding('привет', $charset);
 echo 'encode text: "' . $text . '" charset: ' .  $charset . PHP_EOL;
 echo 'decode text: "' . Word::stringToUtf($text) . '" charset: ' . Word::DEFAULT_CHARSET . PHP_EOL;
 echo PHP_EOL;
+
+// Example: date convert
+// convert month. 2019-01-01 => 01 января 2019
+echo 'date converter: ' . Word::convertDateMonthToWord('2019-01-01') . PHP_EOL;
+// delimiter. 05.05.2018 => 05/мая/2018
+echo 'date converter delimiter: ' . Word::convertDateMonthToWord('05.05.2018', '/') . PHP_EOL;
+echo PHP_EOL;
