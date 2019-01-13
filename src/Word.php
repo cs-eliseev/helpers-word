0f38d5aa60a41aff4564fdb1ed5b45b72e0ba524
cs-eliseev <a.k.eliseev@gmail.com>
2019-01-13 08:13:40 +0300

    add method text transliterate

--- src/Word.php
+++ src/Word.php
@@ -30,6 +30,74 @@ class Word
         '12' => 'декабря',
     ];
 
+    const DICTIONARY_RUS_TO_EN = [
+        'а' => 'a',
+        'б' => 'b',
+        'в' => 'v',
+        'г' => 'g',
+        'д' => 'd',
+        'е' => 'e',
+        'ё' => 'e',
+        'ж' => 'zh',
+        'з' => 'z',
+        'и' => 'i',
+        'й' => 'y',
+        'к' => 'k',
+        'л' => 'l',
+        'м' => 'm',
+        'н' => 'n',
+        'о' => 'o',
+        'п' => 'p',
+        'р' => 'r',
+        'с' => 's',
+        'т' => 't',
+        'у' => 'u',
+        'ф' => 'f',
+        'х' => 'h',
+        'ц' => 'ts',
+        'ч' => 'ch',
+        'ш' => 'sh',
+        'щ' => 'sht',
+        'ь' => '',
+        'ы' => 'y',
+        'ъ' => '',
+        'э' => 'e',
+        'ю' => 'yu',
+        'я' => 'ya',
+        'А' => 'A',
+        'Б' => 'B',
+        'В' => 'V',
+        'Г' => 'G',
+        'Д' => 'D',
+        'Е' => 'E',
+        'Ж' => 'Zh',
+        'З' => 'Z',
+        'И' => 'I',
+        'Й' => 'Y',
+        'К' => 'K',
+        'Л' => 'L',
+        'М' => 'M',
+        'Н' => 'N',
+        'О' => 'O',
+        'П' => 'P',
+        'Р' => 'R',
+        'С' => 'S',
+        'Т' => 'T',
+        'У' => 'U',
+        'Ф' => 'F',
+        'Х' => 'H',
+        'Ц' => 'Ts',
+        'Ч' => 'Ch',
+        'Ш' => 'Sh',
+        'Щ' => 'Sht',
+        'Ь' => '',
+        'Ы' => 'Y',
+        'Ъ' => '',
+        'Э' => 'E',
+        'Ю' => 'Yu',
+        'Я' => 'Ya'
+    ];
+
     /**
      * Convert string charset to UTF-8
      *
@@ -74,5 +142,17 @@ class Word
             $prefix . $worlds[($number % 100 > 4 && $number % 100 < 20) ? 2 : $map[min($number % 10, 5)]],
             $number
         );
+    }
+
+    /**
+     * Text transliterate by dictionary (Rus dictionary)
+     *
+     * @param $text
+     * @param array $dictionary
+     * @return string
+     */
+    public static function transliterate($text, array $dictionary = self::DICTIONARY_RUS_TO_EN): string
+    {
+        return empty($text) ? '' : str_replace(array_keys($dictionary), array_values($dictionary), $text);
     }
 }
