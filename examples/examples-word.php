012a286b0e4ad59dce8db0b4c1a074d5a0524adb
cs-eliseev <a.k.eliseev@gmail.com>
2019-01-27 18:17:38 +0300

    add autoload composer

--- examples/examples-word.php
+++ examples/examples-word.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use cse\helpers\Word;
 
